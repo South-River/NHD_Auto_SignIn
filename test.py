0cbe55c6c5160e5381f64a7b75501d30d7349952
SouthRiver <52597530+South-River@users.noreply.github.com>
2022-01-13 16:48:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,13 @@ from selenium.webdriver.chrome.options import Options
 import os
 
 chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.managed_default_content_settings.images": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument('--incognito')
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
@@ -10,6 +17,5 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
-driver.get("https://www.baidu.com")
-print(driver.title)
-driver.quit()
+
+driver.get('https://v6.nexushd.org')
